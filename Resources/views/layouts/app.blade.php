29c46487d9f1407c619aa3a5e5fa4a2712a500ef
marco76tv <marco.sottana@gmail.com>
2021-07-21 17:21:50 +0200

    .

---
+++ Resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title_prefix', config('bootstrap-italia.title_prefix', ''))
+        @yield('title', config('bootstrap-italia.title', 'Bootstrap-Italia'))
+        @yield('title_postfix', config('bootstrap-italia.title_postfix', ''))</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ Theme::asset('pub_theme::dist/css/app.css') }}" rel="stylesheet">
+
+    @yield('bootstrapitalia_css')
+
+    <script>
+        window.__PUBLIC_PATH__ = '{{ asset('
+        vendor / bootstrap - italia / dist / fonts ') }}'
+
+    </script>
+</head>
+
+<body>
+    @yield('body')
+
+    @yield('bootstrapitalia_js')
+</body>
+
+</html>
